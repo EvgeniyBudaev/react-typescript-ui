d56ab6d1203f898a45483b022e98368e583e98d0
Evgeniy Budaev <budaev.e@gmail.com>
2022-05-24 07:59:17 +0300

    upadate useCopyToClipboard

--- frontend/src/hooks/useCopyToClipboard.ts
+++ frontend/src/hooks/useCopyToClipboard.ts
@@ -11,7 +11,11 @@ function useCopyToClipboard(): [CopiedValue, CopyFn] {
       const textarea = document.createElement("textarea");
       textarea.textContent = text;
       textarea.style.position = "fixed";
+      textarea.style.top = "0";
+      textarea.style.left = "0";
+      textarea.style.opacity = "0";
       document.body.appendChild(textarea);
+      textarea.focus();
       textarea.select();
       try {
         document.execCommand("copy");
